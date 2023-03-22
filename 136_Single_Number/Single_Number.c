e638f8fe34878b38c1a579ce01cdac331df3dfc9
JJR <jiarung1127@gmail.com>
2023-03-22 20:48:33 +0800

    Single_Number 1 version

---
+++ 136_Single_Number/Single_Number.c
@@ -0,0 +1,18 @@
+int singleNumber(int *nums, int numsSize) {
+   for(int i = 0; i < numsSize; i++) {
+       int count = 0;
+       for(int j = 0; j < numsSize; j++) {
+           if(nums[j] == nums[i]) {
+              count++;
+           }
+       }
+       if(count == 1) {
+          return nums[i];
+       }
+   }
+}
+
+
+void main(void) {
+    
+}
