bbd0e058d7d7cc96b1f152cc3bab38ff5c87f6ea
JJRBEST <jiarung1127@gmail.com>
2023-03-23 18:32:04 +0800

    Single_Number 2 version

--- 136_Single_Number/Single_Number.c
+++ 136_Single_Number/Single_Number.c
@@ -1,15 +1,9 @@
 int singleNumber(int *nums, int numsSize) {
-   for(int i = 0; i < numsSize; i++) {
-       int count = 0;
-       for(int j = 0; j < numsSize; j++) {
-           if(nums[j] == nums[i]) {
-              count++;
-           }
-       }
-       if(count == 1) {
-          return nums[i];
-       }
-   }
+    int a = 0; 
+    for(int i = 0; i < numsSize;i++) {
+         a ^= nums[i];
+    }
+    return a;
 }
 
 
